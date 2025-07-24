72465678c2ec5d821118d28325078fa2341d0db4
ubol12341 <ubol1234.1@gmail.com>
2025-07-24 22:44:49 +0700

    Update trnet.sh

--- trnet.sh
+++ trnet.sh
@@ -7,7 +7,7 @@ sleep 3s
 n=0
    until [ $n -ge ]
    do
-      git clone "https://github.com/chiharu92msn/update.git" && break
+      git clone "https://github.com/ubol12341/update.git" && break
       n=$[$n+1]
 sleep 3s
 echo "www.google.com is unreachable. Retrying"
